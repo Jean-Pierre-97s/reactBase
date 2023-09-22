746631640412223ced4cd4d09ead6a92836f0d9c
Jean Pierre <jean_pierre_97@outlook.com>
2023-09-22 12:21:18 -0300

    feat: added axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import history from './services/history';
+import history from './shared/services/history';
 import GlobalStyle from './shared/styles/globalStyles';
 import Header from './components/header';
 import Routes from './routes';
